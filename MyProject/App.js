5043d7a90a35b7270222c42e745d8e0a5b96af88
Vadim Zhukov <v.zhukov@medpoint24.ru>
2021-10-19 16:19:03 +0300

    init

---
+++ MyProject/App.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { View, Text, Image, Button, PermissionsAndroid } from 'react-native';
+import * as ImagePicker from "react-native-image-picker"
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
+const createFormData = (photo, body) => {
+  const data = new FormData();
+
+  data.append('photo', {
+    name: photo.fileName,
+    type: photo.type,
+    uri:
+      Platform.OS === 'android' ? photo.uri : photo.uri.replace('file://', ''),
+  });
+
+  Object.keys(body).forEach((key) => {
+    data.append(key, body[key]);
+  });
+
+  return data;
+};
+
+export default class App extends React.Component {
+  state = {
+    photo: null,
+  };
+
+  handleChoosePhoto = () => {
+    const options = {
+      noData: true,
+    };
+    ImagePicker.launchImageLibrary(options, (response) => {
+      if (response.uri) {
+        this.setState({ photo: response });
+      }
+    });
+  };
+
+  handleMakePhoto = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: 'App Camera Permission',
+        message: 'App needs access to your camera',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log(granted);
+    const options = {
+      maxWidth: 240,
+      maxHeight: 240,
+      noData: true,
+    };
+    ImagePicker.launchCamera(options, res => {
+      this.setState({ photo: res.assets[0] });
+      console.log(res);
+    })
+  }
+
+  handleUploadPhoto = () => {
+    console.log('\n\nYA  V UPLOAD\n\n');
+    // fetch('http://10.0.2.2:3000/api/upload', {
+    fetch('https://masterup.2hst.ru:4000/reports/photos/upload', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE2LCJ1c2VyVHlwZSI6ImNsaWVudCIsImlhdCI6MTYzNDY0ODkzNSwiZXhwIjoxNjM0NjUyNTM1fQ.p_8KbVqBKqLu1gtWg8gahjIxQHdU9klxXAEOas-HEpQ'
+      },
+      body: createFormData(this.state.photo, {}),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log('upload succes', response);
+        alert('Upload success!');
+        this.setState({ photo: null });
+      })
+      .catch((error) => {
+        console.log('upload error', error);
+        alert('Upload failed!');
+      });
+  };
+
+  render() {
+    const { photo } = this.state
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        {photo && (
+          <React.Fragment>
+            <Image
+              source={{ uri: photo.uri }}
+              style={{ width: 300, height: 300 }}
+            />
+            <Button title="Upload" onPress={this.handleUploadPhoto} />
+          </React.Fragment>
+        )}
+        <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
+        <Button title="Make Photo" onPress={this.handleMakePhoto} />
+      </View>
+    )
+  }
+}
---
+++ serv/App.js
@@ -0,0 +1,213 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import type {Node} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob';
+import ReactNativeBlobUtil from 'react-native-blob-util';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const photoUri = 'https://cdnimg.rg.ru/img/content/170/30/99/654_d_850.jpg';
+  const UPLOAD_FOTO_ENDPOINT =
+    'https://masterup.2hst.ru:4000/reports/photos/upload';
+  const TOKEN =
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE2LCJ1c2VyVHlwZSI6ImNsaWVudCIsImlhdCI6MTYzNDU5MTYyNiwiZXhwIjoxNjM0NTk1MjI2fQ.qRbePWkmEkmOYg2SVK2l95QtHK4l_qMp5zVsjxOITjE';
+
+  const onClickUpload = () => {
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+
+    formData.append('image', {
+      uri: photoUri,
+      type: 'image/jpeg',
+      name: 'photo.jpg',
+    });
+    formData.append('text', 'abc');
+
+    formData.append('image1', {
+      type: 'image/png',
+      name: 'photo.png',
+      data: 'abc'
+    });
+
+    // eslint-disable-next-line no-undef
+    const blob = new Blob(['abc'], {
+      type: 'image/jpeg',
+      name: 'photo.jpg',
+      uri: photoUri,
+    });
+
+    formData.append('ttt', blob);
+
+    console.log(formData);
+
+    xhr.open('POST', UPLOAD_FOTO_ENDPOINT);
+    xhr.setRequestHeader('Authorization', TOKEN);
+    xhr.send(formData);
+  };
+
+  const onClickUpload1 = async () => {
+    const formData = new FormData();
+
+    formData.append('image', {
+      uri: photoUri,
+      type: 'image/jpeg',
+      name: 'photo.jpg',
+    });
+    console.log(formData);
+
+    await fetch(UPLOAD_FOTO_ENDPOINT, {
+      method: 'post',
+      body: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data,octet-stream',
+        Authorization: `Bearer ${TOKEN}`,
+        Accept: 'application/json',
+      },
+    });
+  };
+
+  const onClickUpload2 = () => {
+    const formData = new FormData();
+
+    formData.append('image', {
+      uri: photoUri,
+      type: 'image/jpeg',
+      name: 'photo.jpg',
+    });
+    console.log(formData);
+
+    RNFetchBlob.fetch(
+      'POST',
+      UPLOAD_FOTO_ENDPOINT,
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      },
+      formData,
+    ).then(r => console.log);
+  };
+
+  const onClickUpload3 = async () => {
+    const formData = new FormData();
+
+    formData.append('image', {
+      uri: photoUri,
+      type: 'image/jpeg',
+      name: 'photo.jpg',
+    });
+    console.log(formData);
+
+    await ReactNativeBlobUtil.fetch(
+      'POST',
+      UPLOAD_FOTO_ENDPOINT,
+      {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      formData,
+    );
+  };
+
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Button title={'UPLOAD'} onPress={onClickUpload} />
+          <Button
+            title={'UPLOAD1'}
+            onPress={async () => {
+              await onClickUpload1();
+            }}
+          />
+          <Button title={'UPLOAD2'} onPress={onClickUpload2} />
+          <Button title={'UPLOAD3'} onPress={onClickUpload3} />
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
